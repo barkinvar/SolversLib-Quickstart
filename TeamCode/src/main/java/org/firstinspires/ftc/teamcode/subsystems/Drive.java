c6841722e5b6cdfa99534303b2f7f411c4ffcd32
Barkin <varbarkn@gmail.com>
2025-12-09 00:21:04 +0300

    field

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive.java
@@ -16,9 +16,31 @@ public class Drive extends SubsystemBase {
     }
 
     public void joystickDrive(double forward, double strafe, double turn) {
-        mFollower.setTeleOpDrive(forward, strafe, turn, false);
-    }
+        // 1. Get the current heading in radians
+        double heading = mFollower.getPose().getHeading();
 
+        // 2. Calculate Field-Oriented Vectors manually
+        // Since 90 degrees (North) is "Forward", we treat:
+        // 'forward' input as Field Y
+        // 'strafe' input as Field X
+
+        // We rotate these vectors by the negative of the robot's heading to convert
+        // from Field Frame to Robot Frame.
+
+        // Math:
+        // RobotX = FieldX * cos(-heading) - FieldY * sin(-heading)
+        // RobotY = FieldX * sin(-heading) + FieldY * cos(-heading)
+
+        // Subbing in strafe for FieldX and forward for FieldY:
+        double rotForward = strafe * Math.sin(-heading) + forward * Math.cos(-heading);
+        double rotStrafe  = strafe * Math.cos(-heading) - forward * Math.sin(-heading);
+
+        // 3. Send to follower as ROBOT CENTRIC (true)
+        // We pass true because we have already handled the field-centric rotation math above.
+        // Note: Check if your drive setup expects Y to be Left or Right.
+        // Standard Pedro/Roadrunner is X=Forward, Y=Left.
+        mFollower.setTeleOpDrive(rotForward, rotStrafe, turn, true);
+    }
     public void resetHeading() {
         mFollower.startTeleOpDrive();
         mFollower.setPose(new Pose(57.000, 9.000, Math.toRadians(90.0)));
@@ -36,4 +58,8 @@ public class Drive extends SubsystemBase {
     public boolean isAligned() {
         return aligned;
     }
+
+
 }
+
+
